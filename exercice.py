37e087c0f3ee9dd74b6efa393d80c0244c640cb5
aymanechalh <77995760+aymanechalh@users.noreply.github.com>
2021-02-13 13:31:51 -0500

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -4,33 +4,49 @@
 import random
 
 def is_even_len(string):
-	return 0xDEADBEEF
+    if len(string)%2==0:
+        parity='pair'
+    else:
+        parity='imapir'
+
 
 
 def get_num_char(string, char):
-	return 0xDEADBABE
+    a=0
+    for i in string:
+        if i==char:
+            a+=1
+    return a
+
+
 
 
 def get_first_part_of_name(name):
-	return ""
-
-
+    b=name.split("-")
+    c=b[0]
+    e=c.capitalize()
+    print(f" Bonjour {e} ")
+    return e
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
-
+    import random
+    a= random.choice(animals)
+    b=random.choice(adjectives)
+    c=random.choice(fruits)
+    e=print(f"Aujourd’hui, j’ai vu un {a} s’emparer d’un panier {b} plein de {c}.")
+    return e
 
 if __name__ == "__main__":
-	spam = "Bonjour!"
-	parity = "pair" if is_even_len(spam) else "impair"
-	print(f"Le nombre de caractère dans la chaine '{spam}' est {parity}.")
+    spam = "Bonjour!"
+    parity = "pair" if is_even_len(spam) else "impair"
+    print(f"Le nombre de caractère dans la chaine '{spam}' est {parity}.")
 
-	eggs = "Hello, world!"
-	print(f"Le nombre d'occurrence de l dans '{eggs}' est : {get_num_char(eggs, 'l')}.")
+    eggs = "Hello, world!"
+    print(f"Le nombre d'occurrence de l dans '{eggs}' est : {get_num_char(eggs, 'l')}.")
 
-	parrot = "jean-marc"
-	print(f"Pour {parrot}, on a '{get_first_part_of_name(parrot)}'.")
+    parrot = "jean-marc"
+    print(f"Pour {parrot}, on a '{get_first_part_of_name(parrot)}'.")
 
-	animals = ("chevreuil", "chien", "pigeon")
-	adjectives = ("rouge", "officiel", "lourd")
-	fruits = ("pommes", "kiwis", "bananes")
-	print(get_random_sentence(animals, adjectives, fruits))
+    animals = ("chevreuil", "chien", "pigeon")
+    adjectives = ("rouge", "officiel", "lourd")
+    fruits = ("pommes", "kiwis", "bananes")
+    print(get_random_sentence(animals, adjectives, fruits))
